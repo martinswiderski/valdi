44c56ce4bcaa6fcfcd34502e5aa6addf143dd183
Martin Swiderski <codebloke@gmail.com>
2017-03-25 09:39:07 +0000

    version label

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    version: '0.0.373',
-    published: '2017-03-23',
+    version: '0.0.374',
+    published: '2017-03-25',
     readme: 'README.md',
     package: 'package.json'
 };
